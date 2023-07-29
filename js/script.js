5861adca7f096aaee3d5f7ce6493b03031b0e04e
Guilherme Antônio <127310615+guilhermeantonioo@users.noreply.github.com>
2023-07-28 21:12:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const token = "e5b07b138c5057";
 
 function getLocation (){ 
-    fetch("https://ipinfo.io/json").then((Response) => Response.json())
+    fetch(`https://ipinfo.io/json?token=e5b07b138c5057`)
+    .then((Response) => Response.json())
     .then((data) => console.log(data))
     .catch((error) => console.log("Erro ao obter a localização:", error));
 }
